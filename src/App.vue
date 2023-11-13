da1b0853caac99f71aad36a7bd4fb2b2a8b39d74
Linashem <lina.shemeneva@yandex.ru>
2023-11-14 02:29:37 +0300

    first comit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HeaderPage from './components/HeaderPage.vue';
+</script>
+
+<template>
+  
+  <HeaderPage/>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">HEADER</header>
+  <div class="mb"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.header{
+    // display: flex;
+    // justify-content: center;
+    // align-items: center;
+    background-image: url('baner.jpg');
+    min-height:100vh;
+    width: 100vw;
+    // background-size:100% auto;
+    background-size:cover;
+    background-position: center top;
+
+    background-repeat: no-repeat;
+}
+.mb{
+    height: 2000px;
+}
+</style>
